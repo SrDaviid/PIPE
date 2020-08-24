0eac85a3ec8872f09389860ab1877a5b3b440090
SrDaviid <70109934+SrDaviid@users.noreply.github.com>
2020-08-23 19:26:02 -0700

    Add files via upload

---
+++ 6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+           //Maximo: Programa que saca por el STDOUT el maximo numero recibido por el STDIN. 
+          
+        
+        int ma=0, me=0, num=0;
+        Console.WriteLine ("Cuantos numeros va ingresar");
+        int numeros=int.Parse(Console.ReadLine());
+        for(int i =1;i<=numeros;i++){
+        Console.WriteLine ("Digite un numero: ");
+        num=int.Parse(Console.ReadLine()); 
+            
+            if(i==1){
+            ma=num;
+            me=num;
+            }
+            if(num>ma){ma=num;}
+            if(num<me){me=num;}
+            
+            
+            
+        num=0; 
+        }
+        
+        Console.WriteLine (" numero mayor es: "+ma);
+  
+
+       }
+    }        
+}            
+       
+    
+        
+        
+        
+        
+
+
+
+        
+    
